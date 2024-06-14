f4ae3166c6208d4707f7df133e46f7e752bb6d18
AlbertLeipzig <albertclemvill@gmail.com>
2024-06-14 10:55:55 +0200

    work in progress

---
+++ src/main.js
@@ -0,0 +1,7 @@
+console.clear();
+
+let employeeFirstName = "Dora";
+let employeeLastName = "Wagner";
+let employeeEmail = "dora.wagner@mail.com";
+
+console.log({ employeeFirstName, employeeLastName, employeeEmail });
